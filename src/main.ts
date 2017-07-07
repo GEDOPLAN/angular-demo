a71d6cf38ccb2d9c486458acabdd164bb6536658
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2017-07-07 11:41:13 +0200

    i18n

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/merge';
 
@@ -12,4 +12,38 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+function callAjax(url, callback) {
+  ;
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      callback(xmlhttp.responseText);
+    }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+//const locale = navigator.language; > Browsersprache
+let locale;
+const urlLocale = new URLSearchParams(window.location.search).get('locale');
+switch (urlLocale) {
+  case 'en': locale = null; break;
+  default: locale = urlLocale;
+}
+
+
+const url = `http://localhost:4200/assets/messages${locale == null ? '' : '.' + locale}.xlf`;
+callAjax(url, (response) => {
+  platformBrowserDynamic().bootstrapModule(AppModule,
+    {
+      providers: [
+        { provide: TRANSLATIONS, useValue: response },
+        { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+        { provide: LOCALE_ID, useValue: locale }
+      ]
+    });
+});
+
+

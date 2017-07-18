029bf13c1872549b969f3504ed6496ed8586081c
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2017-07-18 11:40:58 +0200

    router animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, trigger, transition, animate, style } from '@angular/core';
 import {SourceLinkServiceService} from './services/source-link-service.service' 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+   animations: [trigger(
+    'openClose',
+    [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate('2000ms', style({ opacity: 1 }))
+      ]),
+      transition(":leave", [
+        animate('2000ms', style({ opacity: 0 }))
+      ])
+    ])]
 })
 export class AppComponent {
 
